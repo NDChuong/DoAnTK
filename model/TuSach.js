e3ba84a480ac4b335afb30ea6426d77d94dcc258
cvdat2097 <33714178+cvdat2097@users.noreply.github.com>
2018-06-07 23:01:21 +0700

    Update TuSach.js funtions
    Return id when add new quest/book

--- model/TuSach.js
+++ model/TuSach.js
@@ -11,9 +11,12 @@ var Node_goc = new DOMParser().parseFromString("<Du_lieu />", "text/xml");
 var duongDan;
 var duLieu;
 
-var _idYeuCau = 0;
+var _idYeuCau=0;
 
 class TuSach {
+  GetIdYeuCau(){
+    return _idYeuCau++;
+  }
   constructor() {
     this.DocDuLieu("DanhSachTuSach.xml");
   }
@@ -100,7 +103,7 @@ class TuSach {
   LayRaToanBoSach() {
     var danhSachSach = duLieu.getElementsByTagName("sach");
     var soLuong = danhSachSach.length;
-    var sach = Node_goc.createElementNS(xmlns_v, "sach");
+    var sach = Node_goc.createElementNS(xmlns_v,"sach");
     for (var i = 0; i < soLuong; i++) {
       sach.appendChild(danhSachSach[i]);
     }
@@ -117,7 +120,7 @@ class TuSach {
     return "null";
   }
 
-  //-----------------------------------END--------------------------------------------
+//-----------------------------------END--------------------------------------------
 
   CapNhatDiemDanhGia(idChu, soLuongDiemCongThem) {
     //số lượng điểm cộng thêm có thể âm hoặc vote up or vote down
@@ -142,53 +145,53 @@ class TuSach {
 
   //----------------------------------YEU CAU-------------------------------------------
 
-  LayRaMotYeuCauChoMuonSach(idYeuCau) {
+  LayRaMotYeuCauChoMuonSach( idYeuCau ){
     var yeuCau = duLieu.getElementsByTagName("yeu_cau_cho_muon");
-    for (var i = 0; i < yeuCau.length; i++) {
-      if (yeuCau[i].getAttribute('id_yeu_cau') == idYeuCau) {
+    for( var i = 0; i<yeuCau.length; i++){
+      if( yeuCau[i].getAttribute('id_yeu_cau')==idYeuCau ){
         return this.ConvertToJson(yeuCau[i]);
       }
     }
     return 'null';
   }
 
-  LayRaToanBoYeuCauChoMuonSach() {
+  LayRaToanBoYeuCauChoMuonSach(){
     var danhSachYeuCauChoMuonSach = duLieu.getElementsByTagName('yeu_cau_cho_muon');
-    var soLuong = danhSachYeuCauChoMuonSach.length;
-    var result = Node_goc.createElementNS(xmlns_v, 'danh_sach');
-    for (var i = 0; i < soLuong; i++) {
+    var soLuong = danhSachYeuCauChoMuonSach.length; 
+    var result = Node_goc.createElementNS(xmlns_v,'danh_sach');
+    for( var i = 0 ; i < soLuong ; i++ ){
       result.appendChild(danhSachYeuCauChoMuonSach[i]);
     }
     return this.ConvertToJson(result);
   }
 
-  LayRaMotYeuCauMuonSach(idYeuCau) {
+  LayRaMotYeuCauMuonSach( idYeuCau ){
     var yeuCau = duLieu.getElementsByTagName("yeu_cau_muon");
-    for (var i = 0; i < yeuCau.length; i++) {
-      if (yeuCau[i].getAttribute('id_yeu_cau') == idYeuCau) {
+    for( var i = 0; i<yeuCau.length; i++){
+      if( yeuCau[i].getAttribute('id_yeu_cau')==idYeuCau ){
         return this.ConvertToJson(yeuCau[i]);
       }
     }
     return 'null';
   }
 
-  LayRaToanBoYeuCauMuonSach() {
+  LayRaToanBoYeuCauMuonSach(){
     var danhSachYeuCauMuonSach = duLieu.getElementsByTagName('yeu_cau_muon');
-    var soLuong = danhSachYeuCauMuonSach.length;
-    var result = Node_goc.createElementNS(xmlns_v, 'danh_sach');
-    for (var i = 0; i < soLuong; i++) {
+    var soLuong = danhSachYeuCauMuonSach.length; 
+    var result = Node_goc.createElementNS(xmlns_v,'danh_sach');
+    for( var i = 0 ; i < soLuong ; i++ ){
       result.appendChild(danhSachYeuCauMuonSach[i]);
     }
     return this.ConvertToJson(result);
   }
 
-  ThemYeuCauChoMuonSach(idChu, idNguoiMuon, idSach, ngayMuon, ngayTra) {
+  ThemYeuCauChoMuonSach(idChu, idNguoiMuon, idSach, ngayMuon, ngayTra,idYeuCau) {
 
     var danhSachTuSach = duLieu.getElementsByTagName("tu_sach");
     for (var i = 0; i < danhSachTuSach.length; i++) {
       if (idChu == danhSachTuSach[i].getAttribute("id_chu")) {
         var Node_moi = Node_goc.createElementNS(xmlns_v, "yeu_cau_cho_muon");
-        Node_moi.setAttributeNS(xmlns_v, "id_yeu_cau", _idYeuCau);
+        Node_moi.setAttributeNS(xmlns_v,"id_yeu_cau",idYeuCau);
         Node_moi.setAttributeNS(xmlns_v, "id_user_muon", idNguoiMuon);
         Node_moi.setAttributeNS(xmlns_v, "id_sach", idSach);
         Node_moi.setAttributeNS(xmlns_v, "ngay_muon", ngayMuon);
@@ -206,13 +209,13 @@ class TuSach {
     }
   }
 
-  ThemYeuCauMuonSach(idChu, idNguoiChoMuon, idSach, ngayMuon, ngayTra) {
+  ThemYeuCauMuonSach(idChu, idNguoiChoMuon, idSach, ngayMuon, ngayTra, idYeuCau) {
 
     var danhSachTuSach = duLieu.getElementsByTagName("tu_sach");
     for (var i = 0; i < danhSachTuSach.length; i++) {
       if (idChu == danhSachTuSach[i].getAttribute("id_chu")) {
         var Node_moi = Node_goc.createElementNS(xmlns_v, "yeu_cau_muon");
-        Node_moi.setAttributeNS(xmlns_v, "id_yeu_cau", _idYeuCau);
+        Node_moi.setAttributeNS(xmlns_v,"id_yeu_cau",idYeuCau);
         Node_moi.setAttributeNS(xmlns_v, "id_user_cho_muon", idNguoiChoMuon);
         Node_moi.setAttributeNS(xmlns_v, "id_sach", idSach);
         Node_moi.setAttributeNS(xmlns_v, "ngay_muon", ngayMuon);
@@ -230,24 +233,24 @@ class TuSach {
     }
   }
 
-  XoaYeuCau(idYeuCau) {//id yeu cau cho muon sach va yeu cau muon sach la 1
-    var danhSachYeuCau = duLieu.getElementsByTagName("yeu_cau_cho_muon");
-    for (var i = 0; i < danhSachYeuCau.length; i++) {
-      if (danhSachYeuCau[i].getAttribute("id_yeu_cau") == idYeuCau) {
+  XoaYeuCau(idYeuCau){//id yeu cau cho muon sach va yeu cau muon sach la 1
+    var danhSachYeuCau=duLieu.getElementsByTagName("yeu_cau_cho_muon");
+    for(var i=0;i<danhSachYeuCau.length;i++){
+      if(danhSachYeuCau[i].getAttribute("id_yeu_cau")==idYeuCau){
         duLieu.removeChild(danhSachYeuCau[i]);
       }
     }
-
-    var danhSachYeuCau = duLieu.getElementsByTagName("yeu_cau_muon");
-    for (var i = 0; i < danhSachYeuCau.length; i++) {
-      if (danhSachYeuCau[i].getAttribute("id_yeu_cau") == idYeuCau) {
+    
+    var danhSachYeuCau=duLieu.getElementsByTagName("yeu_cau_muon");
+    for(var i=0;i<danhSachYeuCau.length;i++){
+      if(danhSachYeuCau[i].getAttribute("id_yeu_cau")==idYeuCau){
         duLieu.removeChild(danhSachYeuCau[i]);
       }
     }
   }
-  //-------------------------------------END------------------------------------------
+//-------------------------------------END------------------------------------------
 
-  //-------------------------------------LICH SU--------------------------------------
+//-------------------------------------LICH SU--------------------------------------
 
   ThemSachVaoLichSuChoMuon(idChu, idNguoiMuon, idSach, ngayMuon, ngayTra) {
     var danhSachTuSach = duLieu.getElementsByTagName("tu_sach");
@@ -256,7 +259,7 @@ class TuSach {
         var danhSachSachTrongLichSu = danhSachTuSach[i]
           .getElementsByTagName("lich_su_cho_muon_sach")[0]
           .getElementsByTagName("sach_cho_muon");
-        var flag = 0;
+        var flag=0;
         for (var j = 0; j < danhSachSachTrongLichSu.length; j++) {
           if (danhSachSachTrongLichSu[j].getAttribute("id_sach") == idSach) {//Nếu sách đã có trong lịch sử
             var Node_moi = Node_goc.createElementNS(xmlns_v, "user_muon");
@@ -268,11 +271,11 @@ class TuSach {
               Node_moi,
               danhSachSachTrongLichSu[j].getElementsByTagName("user")[0]
             ); //chèn vào đầu
-            flag = 1;
+            flag=1;
             break;
           }
         }
-        if (flag == 0) {//nếu sách chưa có trong lịch sử
+        if (flag==0) {//nếu sách chưa có trong lịch sử
           var sach = Node_goc.createElementNS(xmlns_v, "sach_cho_muon");
           sach.setAttributeNS(xmlns_v, "id_sach", idSach);
           var user = Node_goc.createElementNS(xmlns_v, "user_muon");
@@ -296,7 +299,7 @@ class TuSach {
         var danhSachSachTrongLichSu = danhSachTuSach[i]
           .getElementsByTagName("lich_su_muon_sach")[0]
           .getElementsByTagName("sach_muon");
-        var flag = 0;
+        var flag=0;
         for (var j = 0; j < danhSachSachTrongLichSu.length; j++) {
           if (danhSachSachTrongLichSu[j].getAttribute("id_sach") == idSach) {//Nếu sách đã có trong lịch sử
             var Node_moi = Node_goc.createElementNS(xmlns_v, "user_cho_muon");
@@ -308,11 +311,11 @@ class TuSach {
               Node_moi,
               danhSachSachTrongLichSu[j].getElementsByTagName("user_cho_muon")[0]
             ); //chèn vào đầu
-            flag = 1;
+            flag=1;
             break;
           }
         }
-        if (flag == 0) {//nếu sách chưa có trong lịch sử
+        if (flag==0) {//nếu sách chưa có trong lịch sử
           var sach = Node_goc.createElementNS(xmlns_v, "sach_muon");
           sach.setAttributeNS(xmlns_v, "id_sach", idSach);
           var user = Node_goc.createElementNS(xmlns_v, "user_cho_muon");
@@ -397,7 +400,7 @@ class TuSach {
     }
   }
 
-  //------------------------------------END--------------------------------------
+//------------------------------------END--------------------------------------
 
   LayRaMotTuSach(idChu) {
     for (var i = 0; i < duLieu.getElementsByTagName("tu_sach").length; i++) {
