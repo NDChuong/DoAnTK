a9192436c65f9cf0fcf1abb7106e4d4f0fd3b583
cvdat2097 <33714178+cvdat2097@users.noreply.github.com>
2018-06-08 02:06:02 +0700

    Add login form attributes in layout.hbs  for testing

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,27 @@ var router = express.Router();
 var business = require('../controller/business');
 var lib = require('../controller/lib');
 
+router.post('/', function (req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  var user = business.GetAccountInfo(username);
+
+
+  if (user != null && (username == user.id_user && password == user.password)) {
+
+    req.session.userid = username;
+    console.log('Login successfully');
+    res.redirect('/');
+  }
+  else {
+    console.log('login failed');
+    res.send("login failed");
+  }
+
+});
+
+// ======== This GET method is for debugging purpose ONLY
 router.get('/', function (req, res, next) {
   // var username = req.body['username'];
   // var pass = req.body['password'];
@@ -25,6 +46,7 @@ router.get('/', function (req, res, next) {
         console.log('Logged in successfully');
       }
       else {
+        console.log('Login failed');
         res.send("login failed");
       }
     }
@@ -32,24 +54,8 @@ router.get('/', function (req, res, next) {
     res.redirect('/');
   }
 });
-
-router.post('/', function (req, res, next) {
-  var username = req.body.username;
-  var password = req.body.password;
-
-  var user = business.GetAccountInfo(username);
+// ======== This GET method is for debugging purpose ONLY
 
 
-  if (user != null) {
-    if (username == user.id_user && password == user.password) {
-      req.session.userid = username;
-    }
-    else {
-      res.send("login failed");
-    }
-  }
-
-  res.redirect('/');
-});
 
 module.exports = router;
