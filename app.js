b1b6be028ee4ba2bebefc69cd9acb779fa8d6a82
cvdat2097 <33714178+cvdat2097@users.noreply.github.com>
2018-06-10 22:32:04 +0700

    give access to folder '/'

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/')));
 
 // =============== ROUTING ===================
 app.use('/', indexRouter);
